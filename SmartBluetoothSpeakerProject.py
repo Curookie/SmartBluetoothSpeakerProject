f253af7b7ed4a9953e34515c56b8848e66025a8f
Curookie <curookie@naver.com>
2017-06-09 12:27:32 +0900

    Raspberry Pi two HC-SR04 sensor source

---
+++ SmartBluetoothSpeakerProject.py
@@ -0,0 +1,105 @@
+# 컴정과 'Smart 블루투스' 프로젝트 201344052 B반 원종진
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+FIRST_TRIG = 5
+FIRST_ECHO = 6
+SECOND_TRIG = 13
+SECOND_ECHO = 19
+LED_GREEN = 16
+LED_YELLOW = 20
+LED_RED = 21
+
+
+TRIG = [FIRST_TRIG, SECOND_TRIG]
+ECHO = [FIRST_ECHO, SECOND_ECHO]
+distance = [2000, 2000]
+pulse_start = [0, 0]
+pulse_end = [0, 0]
+pulse_duration = [0, 0]
+
+
+
+for i in range(len(ECHO)) :
+    GPIO.setup(TRIG[i], GPIO.OUT)
+    GPIO.setup(ECHO[i], GPIO.IN)
+
+GPIO.setup(LED_GREEN, GPIO.OUT)
+GPIO.setup(LED_YELLOW, GPIO.OUT)
+GPIO.setup(LED_RED, GPIO.OUT)
+
+print("start")
+
+try:
+    while True:
+        for i in range(len(ECHO)):
+            GPIO.output(TRIG[i],False)
+            time.sleep(0.5)
+ 
+            GPIO.output(TRIG[i],True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG[i],False)
+             
+            while GPIO.input(ECHO[i])==0:
+                pulse_start[i]==time.time()
+             
+            while GPIO.input(ECHO[i])==1:
+                pulse_end[i]==time.time() 
+            pulse_duration[i]=pulse_end[i]-pulse_start[i]
+             
+            distance[i]=pulse_duration[i]*17150
+            distance[i]=round(distance[i],2)
+            now = time.localtime()
+            s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+            print(s," Distance : ",distance,"cm")
+except:    
+    GPIO.cleanup()
+
+
+'''
+print("start")
+GPIO.setup(LED_GREEN, GPIO.OUT)
+GPIO.setup(LED_YELLOW, GPIO.OUT)
+GPIO.setup(LED_RED, GPIO.OUT)
+GPIO.setup(FIRST_TRIG, GPIO.OUT)
+GPIO.setup(FIRST_ECHO, GPIO.IN)
+GPIO.setup(SECOND_TRIG, GPIO.OUT)
+GPIO.setup(SECOND_ECHO, GPIO.IN)
+
+GPIO.output(LED_GREEN, False)
+GPIO.output(LED_YELLOW, False)
+GPIO.output(LED_RED, False)
+
+try:
+    while True:
+        GPIO.output(FIRST_TRIG, False)
+        GPIO.output(SECOND_TRIG, False)
+        time.sleep(0.5)
+        GPIO.output(FIRST_TRIG, True)
+        GPIO.output(SECOND_TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(FIRST_TRIG, False)
+        GPIO.output(SECOND_TRIG, False)
+        while GPIO.input(echo) == 0 :
+            pulse_start = time.time()
+        while GPIO.input(echo) == 1 :
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17000
+        distance = round(distance, 2)
+        if(distance<=20):
+            pwmRed.ChangeDutyCycle(100-distance*5)
+        else:
+            pwmRed.ChangeDutyCycle(0)
+        print("Distance : ", distance, "cm")
+
+        
+except :
+    print("Cleanning UP")
+    GPIO.cleanup()
+        
+'''
